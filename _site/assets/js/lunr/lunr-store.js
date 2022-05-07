e76165ad4be766691f5b8bfbedf969c4babeb7f5
Yoonyeong Lee <ynlee1@seegene.com>
2022-05-08 02:10:13 +0900

    [feat] remove unnecessary files & add .gitignore

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "minimal-mistake 를 이용한 GitHub Blog 설치",
-        "excerpt":"Minimal-mistake 장점: 커스터마이징의 장점을 가지고 있으며, 자유도가 높음 What is Jekyll? 텍스트 변환 엔진. Markup language로 글을 작성하면 미리 정의해 놓은 규칙에 따라 정적 웹사이트를 만들어 준다. Markup language? Tag등을 이용하여 문서나 data 구조를 명기하는 언어 How to install Ruby download Jekyll이 32bit이기 때문에 x86으로 설치 with devkit으로 설치 Download...","categories": ["github"],
-        "tags": ["github","Jekyll"],
-        "url": "/github/create-github-blog/",
-        "teaser": null
-      }]
